9ed5b8684c6a6154be1edcb9ab0d62fb222d0084
Walthzer <19926813+Walthzer@users.noreply.github.com>
2024-05-08 09:26:42 +0200

    Update main.c

--- libraries/vl53l0x/main.c
+++ libraries/vl53l0x/main.c
@@ -15,6 +15,12 @@
 extern int vl53l0x_example_single();
 extern int vl53l0x_example_dual();
 
+/** This Example program REQUIRES ALL OF:
+ * - single.c
+ * - dual.c
+ * To be present.
+**/
+
 int main(void) {
   	pynq_init();
 
@@ -24,18 +30,19 @@ int main(void) {
 	switchbox_set_pin(IO_AR_SDA, SWB_IIC0_SDA);
 	iic_init(IIC0);
 
-	//Test Scripts: Select ONE!
-	// - Single Sensor -> vl53l0x_example_single();
-	// - Dual Sensor -> vl53l0x_example_dual();
+	/**Test Scripts: Select ONE!
+	 * - Single Sensor -> vl53l0x_example_single();
+	 * - Dual Sensor -> vl53l0x_example_dual();
+	**/	
 
-	//Connect one sensor to the IIC bus and enjoy!
+	/** Connect one sensor to the IIC bus and enjoy! **/
 	vl53l0x_example_single();
 
-	//Connect two sensors to the IIC bus
-	//ONLY CONNECT ONE TO 5V and GND
-	//--If the second sensor is connected to either 5v or GND
-	//--Before the first is initialized, they conflict.
-	//Run the program and follow instructions!
+	/** Connect two sensors to the IIC bus
+	 * ONLY CONNECT ONE TO 5V and GND
+	 * --If the second sensor is connected to either 5v or GND
+	 * --Before the first is initialized, they conflict.
+	 * Run the program and follow instructions! **/
 	//vl53l0x_example_dual();
 
 	iic_destroy(IIC0);
